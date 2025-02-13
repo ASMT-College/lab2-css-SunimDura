6ecc22e33ed131c4327c300a53ff2a12524d8393
SunimDura <sunimdura60@gmail.com>
2025-02-13 17:46:40 +0545

    flex

---
+++ css flexbox/flexproperty.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center; 
+  height: 150px;
+  background-color: lightblue;
+}
+
+.box {
+  width: 50px;
+  height: 50px;
+  background-color: white;
+  border: 1px solid black;
+  text-align: center;
+  line-height: 50px;
+}
+</style>
+</head>
+<body>
+
+<div class="container">
+  <div class="box">1</div>
+  <div class="box">2</div>
+  <div class="box">3</div>
+</div>
+
+</body>
+</html>
---
+++ css flexbox/grow.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</html>
+
+</style>
+</head>
+<body>
+
+<h1>Create a Flex Container</h1>
+<div class="flex-container">
+  <div style="flex-grow: 1">1</div>
+  <div style="flex-grow: 1">2</div>
+  <div style="flex-grow: 5">3</div>
+  <div style="flex-grow: 1">4</div>
+</div> 
+</div>
+</body>
+</html>
